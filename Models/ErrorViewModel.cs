ce56b4a4a97a4bc063e7990f4a8d94418fe06637
Thimathi Manahara <manahara@ehealthcare.lk>
2025-10-15 10:10:22 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace CoopHospitalHRM.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
